7d4362c127c4c261e30cea87149b550dfc98683b
a2wd <github@a2wd.com>
2016-10-14 00:58:43 +0100

    Updated db tests for crud operations

--- tests/db.tests.js
+++ tests/db.tests.js
@@ -2,6 +2,10 @@ var assert = require("assert")
 var db = require("../server/db")
 var Idea = require("../server/models/Idea")
 
+var writeConcern = {w:1}
+var title = "test title"
+var test_idea = new Idea(title, "test description", ["test comment"])
+
 describe("database", function(){
 
 	describe("basic crud", function(){
@@ -22,12 +26,70 @@ describe("database", function(){
 			assert.ok(ideas)
 		})
 
-		it("should be able to write a record", function(){
-			var test_idea = new Idea()
+		it("should be able to write a record", function(done){
+			//Arrange
+			var ideas_collection = db.collection("ideas")
 
-			
+			//Act
+			ideas_collection.insert(test_idea, writeConcern, function(err, res){
 
+				//Assert
+				assert(err === null)
+				assert.ok(res)
+				assert.equal(res.result.n, 1)
 
+				done()
+			})
+		})
+
+		it("should be able to read a record", function(done){
+			//Arrange
+			var ideas_collection = db.collection("ideas")
+
+			//Act
+			ideas_collection.findOne({title: title}, function(err, item){
+				//Assert
+				assert(err === null)
+				assert.ok(item)
+
+				assert.equal(item.title, title)
+
+				done()
+			})
+		})
+
+		it("should be able to update a record", function(done){
+			//Arrange
+			var ideas_collection = db.collection("ideas")
+			var update = {description: "updated field"}
+
+			//Act
+			ideas_collection.updateOne({title: title}, {$set: update}, function(err, res){
+				//Assert
+				assert(err === null)
+				assert.ok(res)
+
+				assert.equal(res.result.n, 1)
+
+				done()
+			})
+		})
+
+		it("should be able to delete a record", function(done) {
+			//Arrange
+			var ideas_collection = db.collection("ideas")
+
+			//Act
+			ideas_collection.deleteOne({title: title}, function(err, res){
+				//Assert
+				assert(err === null)
+				assert.ok(res)
+
+				assert.equal(res.result.n, 1)
+
+				done()
+			})
+		
 		})
 
 		after(function(){
