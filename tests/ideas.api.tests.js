0f96a388780743c22374d70fe58eee08c34e30a5
a2wd <github@a2wd.com>
2016-10-24 17:34:09 +0100

    Updated ideas api tests

--- tests/ideas.api.tests.js
+++ tests/ideas.api.tests.js
@@ -3,18 +3,28 @@ var ideasApi = require("../server/api.idea.js")
 var Idea = require("../server/models/Idea")
 
 var writeConcern = {w:1}
-var title = "test title"
+var title = "test title [" + Date.now() + "]"
 var test_idea = new Idea(title, "test description", ["test comment"])
 
 describe("ideas db-api", function(){
-
-	it("should be able to insert an idea object", function(done){
+	it("should have a non-negative number of entries", function(){
 		//Arrange
-		var test_idea = new Idea(title, "test description", ["test comment"])
+		//Not needed
+
+		//Act
+		var idea_count = ideasApi.count()
+
+		//Assert
+		assert(idea_count > -1)
+	})
+
+	it("should be able to insert an idea object", function(){
+		//Arrange
 		var expected_count = ideasApi.count() + 1
 
 		//Act
-		var status = ideasApi.create(test_idea);
+		var status = ideasApi.create(test_idea)
+		var idea_count = ideasApi.count()
 
 		//Assert
 		assert.ok(status)
@@ -26,18 +36,50 @@ describe("ideas db-api", function(){
 		var expected_count = ideasApi.count()
 
 		//Act
-		var ideas_objects = ideasApi.read()
+		var ideas_objects = ideasApi.readAll()
 
 		//Assert
 		assert.equal(ideas_objects.length, expected_count)
 	})
 
-	it("should be able to update an entry", function(){
+	it("should be able to list an idea entry by title", function(){
+		//Arrange
+		var search_object = {title: title}
 
+		//Act
+		var idea_object = ideasApi.read(search_object)
+
+		//Assert
+		assert.ok(idea_object)
+		assert.deepEqual(idea_object, test_idea)
+	})
+
+	it("should be able to update an entry", function(){
+		//Arrange
+		var search_object = {title: title}
+
+		var updated_object = new Idea(title, "An updated description", ["test comment"])
+		var update = {description: "An updated description"}
+
+		//Act
+		var api_response = ideaApi.update(search_object, update)
+
+		//Assert
+		assert.ok(api_response)
+		assert.deepEqual(api_response, updated_object)
 	})
 
 	it("should be able to delete an idea entry", function(){
-		
+		//Arrange
+		var search_object = {title: title}
+
+		//Act
+		var status = ideaApi.delete(search_object)
+		var deleted_object = ideaApi.read(search_object)
+
+		//Assert
+		assert.ok(status)
+		assert(deleted_object === null)
 	})
 
 })
