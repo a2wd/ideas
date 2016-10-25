b029696ff6202f9bbadd27d373c8bf456116b4ff
a2wd <github@a2wd.com>
2016-10-25 10:27:32 +0100

    Updated to backend tests

--- server/backend.js
+++ server/backend.js
@@ -1,5 +1,19 @@
+var http = require("http")
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {"Content-Type": "text/plain"})
+	res.end("Hello, world\n")
+})
+
+var listenFunction = function() {
+	server.listen.apply(server, arguments)
+}
+
+var closeFunction = function(callback) {
+	server.close(callback)
+}
+
 module.exports = {
-	hi: function() {
-		console.log("hi")
-	}
+	listen: listenFunction,
+	close: closeFunction
 }
--- tests/backend.tests.js
+++ tests/backend.tests.js
@@ -1,9 +1,58 @@
 var assert = require("assert")
+var http = require("http")
+var server = require("../server/backend")
+
+var port = 8080
+var url = "http://localhost:" + port.toString()
 
 describe("backend", function(){
 	describe("server", function(){
-		it("should listen on port 8080", function(){
-			assert.equal(1,1)
+
+		before(function(){
+			server.listen(port)
 		})
+
+		describe("/ (server root path)", function() {
+
+			it("should return a status code of 200", function(done){
+				//Arrange
+				//Not required
+
+				//Act
+				http.get(url, function(res){
+
+					//Assert
+					assert.equal(200, res.statusCode)
+					done()
+				})
+			})
+
+			it("should say hello world", function(done){
+				//Arrange
+				var data = ""
+				var expected_response = "Hello, world\n"
+
+				//Act
+				http.get(url, function(res){
+					res.on("data", function(chunk) {
+						data += chunk
+					})
+
+					res.on("end", function(){
+						//Assert
+						assert.equal(data, expected_response)
+						done()
+					})
+				})
+
+			})
+
+		})
+
+		after(function(){
+			server.close()
+		})
+
 	})
+
 })
